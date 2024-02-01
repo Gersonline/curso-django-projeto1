7ed63520effacc0f9701d50377aa6b0e96cac0a9
Gerson Lima <gersonline@outlook.com.br>
2024-01-31 22:34:55 -0300

    Testando o context e content da response no template

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 class RecipeViewsTest(TestCase):
     def test_recipe_detail_view_function_is_correct(self):
@@ -41,3 +42,49 @@ class RecipeViewsTest(TestCase):
             '<h1>No recipes found here</h1>',
             response.content.decode('utf-8')
         )
+    
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            '<h1>No recipes found here</h1>',
+            response.content.decode('utf-8')
+        )
+
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
+
+        
+        #self.assertEqual(response_recipes.first().title, 'Recipe Title')
+        
+        pass
